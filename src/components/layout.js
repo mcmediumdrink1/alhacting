1ea10a06dd0bf416ec908aba7f452a8bb6aca8b9
Nic Icaza <Nicaza@Nicaza0149MBP.local>
2020-10-12 23:13:03 -0500

    First commit

---
+++ src/components/layout.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "gatsby"
+const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
+
+export default function Layout({ children }) {
+  return (
+    <div style={{ margin: `3rem auto`, padding: `0 3rem` }}>
+      <header style={{ marginBottom: `1.5rem` }}>
+        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+          <h3 style={{ display: `inline` }}>Amy Heller</h3>
+        </Link>
+        <ul style={{ listStyle: `none`, float: `right` }}>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/about/">About</ListLink>
+          <ListLink to="/contact/">Contact</ListLink>
+        </ul>
+      </header>
+      {children}
+    </div>
+  )
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
 import React from "react"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default function About() {
   return (
+    <Layout>
     <div style={{ color: `teal` }}>
       <Header headerText="Amy Heller" />
       <Header headerText="Actress. Writer. Producer." />
@@ -10,5 +12,6 @@ export default function About() {
           Learn more about my project, <a href="https://www.gofundme.com/f/my-little-renaissance-girl-film"> "My Little Renaissance Girl" </a>
       </p>
     </div>
+    </Layout>
   )
 }
